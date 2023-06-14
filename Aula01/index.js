d05470ff3357f1429d568aa17eabf7e29b42ad17
lucasmdct <lucasmedeiroscosta07@gmail.com>
2023-06-14 20:41:46 -0300

    Adicionando arquivos

--- Aula01/index.js
+++ Aula01/index.js
@@ -3,3 +3,13 @@
 console.log("Hello Node JS");
 console.log("Minha Primeira Mensagem");
 /////////////////////////////////////////
+
+const datefns = require('date-fns');
+
+const dataAtual = new Date();
+
+console.log(dataAtual);
+
+const dataFormatada = datefns.format(dataAtual, 'dd/MM/yyyy');
+
+console.log(dataFormatada);
