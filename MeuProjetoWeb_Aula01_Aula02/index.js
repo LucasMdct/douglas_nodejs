5d5a571d347feac580495e711374a1e89dc35b17
lucasmdct <lucasmedeiroscosta07@gmail.com>
2023-06-15 22:10:45 -0300

    Aula 02 finalizando

--- MeuProjetoWeb_Aula01_Aula02/index.js
+++ MeuProjetoWeb_Aula01_Aula02/index.js
@@ -5,15 +5,67 @@ const porta = 3000;
 
 const servidor = express();
 
-servidor.use("/", (request, response) => {
-    const nome = request.query.nome;
-    const idade = request.query.idade;
+servidor.use(express.json());
 
-    response.setHeader('content-type', 'text/html; charset=utf-8');
+
+//autenticação de middle wares 
+
+servidor.use((request, response, next) => {
+     const token = request.headers.token;
+
+     if(token == 'meu-token'){
+        next();
+     }else{
+        response.status(403);
+        response.send('Token invalido');
+     }
+});
+
+
+// servidor.use("/", (request, response) => {
+//     const nome = request.query.nome;
+//     const idade = request.query.idade;
+
+//     response.setHeader('content-type', 'text/html; charset=utf-8');
+//     response.status(200);
+//     response.send('Ola Express !!! Bem vindo ' + nome + ', Sua idade é ' + idade);
+// });
+
+servidor.get('/usuario/:usuarioId', (request, response) => {
+    const usuarioId = request.params.usuarioId;
+
     response.status(200);
-    response.send('Ola Express !!! Bem vindo ' + nome + ', Sua idade é ' + idade);
+
+    const dadosRespota = {
+        id: usuarioId,
+    };
+
+    response.json(dadosRespota);
+
+});
+
+servidor.post('/usuarios', (request, response) => {
+
+    console.log(request);
+    const conteudo = request.body;
+
+    response.status(201); // um status de criação de registro (201: resposta de êxito do método POST ) 
+    //restful para boas praticas de programaçao
+
+    //sel *  from
+
+    console.log(conteudo);
+
+    //insert into database;
+
+    const conteudoResponsa = {
+        usuario  : conteudo
+    }
+
+    response.json(conteudo)
+
 });
 
 servidor.listen(porta, () => {
-console.log('Servidor iniciado na porta:', porta);
+    console.log('Servidor iniciado na porta:', porta);
 });
--- MeuProjetoWeb_Aula01_Aula02/index_aula01.js
+++ MeuProjetoWeb_Aula01_Aula02/index_aula01.js
@@ -1,52 +1,52 @@
 
 // Importe o módulo http e inicie o serviço na porta desejada //
-const fs = require('fs');
+// const fs = require('fs');
+// const http = require('http');
+// const porta = 30000;
+// // exemplo  2 //
+// function receberRequisicao(request, response) {
+//     response.setHeader('content-type', 'text/html; charset=utf-8');
+//     response.writeHead(200);
+//     response.write('Olá nodejs');
+//     response.end();
+// };
+
+// const servidor = http.createServer(receberRequisicao);
+
+// servidor.listen(porta, () => {
+//     console.log("Servidor Iniciado com sucesso na porta: ", porta);
+// });
+
+// ///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
 const http = require('http');
-const porta = 30000;
-// exemplo  2 //
-function receberRequisicao(request, response) {
-    response.setHeader('content-type', 'text/html; charset=utf-8');
-    response.writeHead(200);
-    response.write('Olá nodejs');
-    response.end();
-};
+const fs = require('fs');
 
-const servidor = http.createServer(receberRequisicao);
+const server = http.createServer((req, res) => {
+  // Lendo o arquivo HTML
+  fs.readFile('html/node.html', 'utf8', (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end('Erro interno do servidor');
+      return;
+    }
 
-servidor.listen(porta, () => {
-    console.log("Servidor Iniciado com sucesso na porta: ", porta);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data); // Envia o conteúdo HTML como resposta
+  });
 });
 
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-
-// const http = require('http');
-// const fs = require('fs');
-
-// const server = http.createServer((req, res) => {
-//   // Lendo o arquivo HTML
-//   fs.readFile('html/node.html', 'utf8', (err, data) => {
-//     if (err) {
-//       res.statusCode = 500;
-//       res.end('Erro interno do servidor');
-//       return;
-//     }
-
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.end(data); // Envia o conteúdo HTML como resposta
-//   });
-// });
-
-// const port = 3000;
-// server.listen(port, () => {
-//   console.log(`Servidor está executando em http://localhost:${port}`);
-// });
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor está executando em http://localhost:${port}`);
+});
